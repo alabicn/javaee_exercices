5a63f41015f0d5405bd08b2e11038acc1b33a957
alabicn <alabicn@hotmail.com>
2020-09-20 16:33:45 +0200

    First commit

---
+++ WebContent/WEB-INF/doGet.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>TP1 - doGet</title>
+</head>
+<body>
+	<p>doGet</p>
+</body>
+</html>
---
+++ src/com/tp1/servlets/Requests.java
@@ -0,0 +1,75 @@
+package com.tp1.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Requests")
+public class Requests extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Requests() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher("/WEB-INF/doGet.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"utf-8\" />");
+        out.println("<title>doPost</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>doPost</p>");
+        out.println("</body>");
+        out.println("</html>");
+	}
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"utf-8\" />");
+        out.println("<title>doPut</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>doPut</p>");
+        out.println("</body>");
+        out.println("</html>");
+	}
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"utf-8\" />");
+        out.println("<title>doDelete</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>doDelete</p>");
+        out.println("</body>");
+        out.println("</html>");
+	}
+
+}
